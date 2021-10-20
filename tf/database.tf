a6f75675cd61c084b02da46fd1a463166a87b3bd
soonbeom <gnosia93@naver.com>
2021-10-20 09:30:35 +0900

    ..

--- tf/database.tf
+++ tf/database.tf
@@ -90,6 +90,9 @@ sudo apt-get update
 
 # Install selected packages
 sudo apt-get -y install edb-as13-server 
+
+sudo -u enterprisedb sed -i -e "/is for Unix domain socket connections only/a\local   all             all                        md5" /etc/edb-as/13/main/pg_hba.conf
+sudo -u enterprisedb echo "host    all             all             0.0.0.0/0               md5" >> /etc/edb-as/13/main/pg_hba.conf
 _DATA
     
     tags = {
